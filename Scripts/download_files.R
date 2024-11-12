2d551d599e73c9d9ba262b9572b9f457ea8e8015
Emily Ryznar <emily.ryznar@nmfs.local>
2024-11-12 10:14:31 -0800

    Final downloading script

--- Scripts/download_files.R
+++ Scripts/download_files.R
@@ -4,58 +4,93 @@
 
 library(stringr)
 
-# Set path and read in url + filename csv
+# Read in url + filename csv
+urls <- read.csv("./Data/updated_noaa_signed_urls.csv")
 
-path <- c("//akc0ss-n086/RACE_SAP/CMIIP6 downscaled models/")
-
-urls <- read.csv("./Data/full_noaa_dl_url_list.csv")
-
-# Try breaking filenames down
+# Breakdown/summarize filenames ----
+# specify data frame 
 file.sum <- data.frame()
 
-for(ii in 1:nrow(try)){
+# run loop
+for(ii in 1:nrow(urls)){
   # identify locations of "/" in file name
-  lst <- gregexpr('/', try$name[ii], fixed = TRUE)
+  lst <- gregexpr('/', urls$name[ii], fixed = TRUE)
   
   # extract information based on "/" positions
-  region <- substring(try$name[ii], sapply(lst, `[`, 1) + 1, sapply(lst, `[`, 2) - 1)
-  pathway <- substring(try$name[ii], sapply(lst, `[`, 2) + 1, sapply(lst, `[`, 3) - 1)
-  model <- substring(try$name[ii], sapply(lst, `[`, 3) + 1, sapply(lst, `[`, 4) - 1)
-  variable <- substring(try$name[ii], sapply(lst, `[`, 4) + 1, sapply(lst, `[`, 5) - 1)
+  region <- substring(urls$name[ii], sapply(lst, `[`, 1) + 1, sapply(lst, `[`, 2) - 1)
+  pathway <- substring(urls$name[ii], sapply(lst, `[`, 2) + 1, sapply(lst, `[`, 3) - 1)
+  model <- substring(urls$name[ii], sapply(lst, `[`, 3) + 1, sapply(lst, `[`, 4) - 1)
+  variable <- substring(urls$name[ii], sapply(lst, `[`, 4) + 1, sapply(lst, `[`, 5) - 1)
 
-  # replace all "_" with "/"
-  dat <- substring(try$name[1], sapply(lst, `[`, 5))
+  # identify locations of "_" after "/"
+  dat <- substring(urls$name[ii], sapply(lst, `[`, 5))
   
-  # identify locations of "/" in file name
   lst2 <- gregexpr('_', dat, fixed = TRUE)
   
-  # extract information based on "/" positions
-  model.name <- str_extract(str_extract(dat, '(?<=/).*(?=\\_)'), '(?<=).*(?=\\_r)')
-  model.run <- substring(dat, sapply(lst2, `[`, 1) + 1, sapply(lst2, `[`, 2) - 1)
-  sd.ba <- substring(dat, sapply(lst2, `[`, 2) + 1, sapply(lst2, `[`, 3) - 1)
-  final.raw <- str_detect(dat, " sd+ba ")
-  depth <- substring(dat, sapply(lst2, `[`, 5) + 1, sapply(lst2, `[`, 7) - 1)
-  last.bit <- substring(dat, sapply(lst2, `[`, 7)+1)
+  # ensemble model run file names are different, specify position of information based on this
+ 
+  if(model == "ensemble"){
+    perc.stat <- ifelse(str_detect(dat, "perc") == TRUE, "perc", "stats")
+    model.run = "ensemble"
+    final.raw = NA
+  }else{
+    perc.stat <- NA
+    model.run <- substring(dat, sapply(lst2, `[`, 1) + 1, sapply(lst2, `[`, 2) - 1)
+    final.raw <- ifelse(str_detect(dat, "final") == TRUE, "final", "raw")
+  }
+  
+  # extract the rest of the information using str_detect
+  sd.ba <- ifelse(str_detect(dat, "sd+") == TRUE, "sd+ba", NA)
+  depth <- ifelse(str_detect(dat, "surface") == TRUE, "surface", "bottom")
+  
+ 
   
   # bind information into summary table
-  file.sum <- rbind(file.sum, data.frame(region = region,
-                                         pathway = pathway,
-                                         model = model,
-                                         variable = variable,
-                                         model.name = model.name,
+  file.sum <- rbind(file.sum, data.frame(index = row(urls)[ii],
+                                         region,
+                                         pathway,
+                                         model,
+                                         variable,
                                          model.run,
                                          sd.ba,
                                          final.raw,
                                          depth,
-                                         last.bit))
+                                         perc.stat))
   
 }
 
-try <- urls[1:4,]
-region <- stringr::str_extract(try$name, '(?<=/).*(?=\\/ssp)')
-pathway <- stringr::str_extract(try$name, '(?<=/).*(?<=/).')
+# save csv
+file.sum %>%
+  group_by(pathway, model, variable, depth) %>%
+  reframe(total = n()) -> file.sum2
 
-lst1 <- gregexpr('/', try$name, fixed = TRUE)
-substring(try$name, sapply(lst1, `[`, 3) + 1, sapply(lst1, `[`, 4) - 1)
+write.csv(file.sum2, "./Output/file.summary.csv")
 
-#[1] "0.0"  "0.02" "0.1" 
+urls$name[2] -> tt
+str_after_last(tt, "/") -> kk
+
+
+# Download files ----
+# set path/folder to store downloaded models
+path <- c("//akc0ss-n086/RACE_SAP/CMIIP6 downscaled models/")
+
+# Loop through file names/downloads
+for(ii in 135:nrow(urls)){
+  print(paste0("Downloading file #", row(urls)[ii]))
+  
+  # identify locations of "/" in file name
+  lst <- gregexpr('/', urls$name[ii], fixed = TRUE)
+  
+  # extract information based on "/" positions
+  region <- substring(urls$name[ii], sapply(lst, `[`, 1) + 1, sapply(lst, `[`, 2) - 1)
+  pathway <- substring(urls$name[ii], sapply(lst, `[`, 2) + 1, sapply(lst, `[`, 3) - 1)
+  
+  urls$name[ii] -> tt
+  str_after_last(tt, "/") -> kk
+  
+  download.file(urls$url[ii], destfile = paste0(path, region, "_", pathway, "_", kk))
+}
+
+options(timeout = 5000)
+
+
